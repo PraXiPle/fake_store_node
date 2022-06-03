24b0aff7f0d0f2dba3366e103a32140eae9b4120
yuda akiva <yuda05246@gmail.com>
2022-06-03 11:53:35 +0300

    adding router for users

---
+++ BL/userBL.js
@@ -0,0 +1,66 @@
+const userController = require('../DL/controllers/userController')
+
+async function getUserById(id) {
+
+    const user = await userController.findById(id)
+
+    if (!user) throw 'no such user'
+
+    return user
+}
+
+
+async function getUsers(filter) {
+    const users = await userController.find(filter)
+
+    if (users.length == 0) throw 'no users has been found '
+
+    return users
+
+}
+
+async function creatUser(user) {
+    try {
+        const newUser = await userController.create(user)
+
+        return newUser
+
+    } catch (error) {
+
+        throw 'error adding new user'
+
+    }
+
+}
+
+async function updateUseById(id, newData) {
+    const user = await getUserById(id)
+    delete newData._id
+
+    delete newData.isActive
+
+    try {
+        const updatedUse = await userController.update(id, newData)
+        return updatedUse
+
+    } catch (error) {
+
+        throw 'error updating the user'
+    }
+}
+
+
+async function deleteUserById(id) {
+
+    const user = await getUserById(id)
+
+    try {
+        const deletedUser = await userController.del(id)
+        return deletedUser
+    } catch (error) {
+        throw 'error while deleting user '
+    }
+
+}
+
+module.exports = { deleteUserById, updateUseById, creatUser, getUsers, getUserById }
---
+++ routers/usersRouter.js
@@ -0,0 +1,71 @@
+const userBL = require('../BL/userBL')
+
+
+function Router(app) {
+
+    app.get('/user', (req, res) => {
+        try {
+            const user = userBL.getUserById(req.query.id)
+            res.status(200).send(user)
+        } catch (error) {
+            res.status(404).send()
+        }
+
+    })
+
+    app.post('/users', (req, res) => {
+        try {
+            const users = userBL.getUsers(req.body)
+            res.status(200).send(users)
+        } catch (error) {
+            res.send(error.message)
+        }
+    })
+
+
+    app.post('/user/register', async (req, res) => {
+        try {
+            const user = await userBL.creatUser(req.body)
+            res.status(200).send(user)
+        } catch (error) {
+            res.status(404).send(error)
+        }
+    })
+
+
+    app.put('/user/edit', async (req, res) => {
+        try {
+            const user = await userBL.updateUseById(req.query.id, req.body)
+            res.status(200).send(user)
+
+        } catch (error) {
+
+            res.status(404).send(error)
+        }
+    })
+
+
+    app.delete('/user/delete', async (req, res) => {
+        try {
+            const user = await userBL.deleteUserById(req.query.id)
+            res.status(200).send(user)
+        } catch (error) {
+            res.status(404).send(error)
+        }
+    })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
